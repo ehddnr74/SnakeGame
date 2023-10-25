461c4bef95b4acf37ad4c50eb0c8992f6c0e96cd
ehddnr74 <dol709698@naver.com>
2023-10-25 18:45:25 +0900

    message

---
+++ SnakeGame/main.cpp
@@ -0,0 +1,14 @@
+#include "Application.h"
+
+Application application;
+
+int main()
+{
+    _wsetlocale(LC_ALL, L"korean");
+
+    application.Initialize();
+    application.Run();
+    application.Release();
+
+    return 0;
+}
